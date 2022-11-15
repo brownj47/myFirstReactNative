6d08c1d98d2968c3d45b81b8c9788e4e40c98752
Justus B <justus.c.brown@gmail.com>
2022-11-15 11:55:49 -0800

    github pages branch

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import HeaderBar from "./HeaderBar";
-import MonImg from "./MonImg";
-import MonStats from "./MonStats";
-import NavBar from "./NavBar";
-import NewMonBtn from "./newMonBtn";
-
-export {HeaderBar, MonImg, MonStats, NavBar, NewMonBtn}
--- src/screens/index.js
+++
@@ -1,5 +0,0 @@
-import Home from "./Home";
-import Profile from "./Profile";
-import Credits from "./Credits"
-
-export {Profile, Home, Credits}
